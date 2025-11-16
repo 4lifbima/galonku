de1a46e7705367c6f58ded0db23f1f79aa0e3057
4lifbima <alif67916@gmail.com>
2025-11-17 02:23:02 +0800

    lagi

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
 const API_BASE =
-  process.env.NODE_ENV === 'production' ? 'https://backends.kesug.com/' : 'http://localhost:3000'
+  process.env.NODE_ENV === 'production'
+    ? 'http://localhost/backend' // jika pakai XAMPP di port 80
+    : // ? 'http://localhost:8000'  // jika pakai php -S
+      'https://backends.kesug.com'
 
 export default API_BASE
