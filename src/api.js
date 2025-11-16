e997cf5fbf4460b042430c34f9a67dfb196c470d
4lifbima <alif67916@gmail.com>
2025-11-17 01:59:32 +0800

    commit API

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ const API_BASE =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost/backend' // jika pakai XAMPP di port 80
     : // ? 'http://localhost:8000'  // jika pakai php -S
-      'https://yourdomain.com/backend'
+      'https://backends.kesug.com/'
 
 export default API_BASE
